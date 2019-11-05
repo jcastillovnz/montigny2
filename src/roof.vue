96139c0563ac0075c10b54eed3848c76b412ce48
Jose Castillo <k.y.258@hotmail.com>
2019-11-04 23:19:22 -0300

    Initial commit

---
+++ src/roof.vue
@@ -0,0 +1,56 @@
+<template>
+  <div >
+
+
+
+ 
+  <div id="app" class="wrapper center">
+    <VueProductSpinner :class="'renders'"  :images="imgs" :infinite="true" :slider="false">
+ Cargando
+    </VueProductSpinner>
+  </div>
+
+
+</div>
+
+
+</template>
+
+<style scoped>
+
+</style>
+<script>
+import VueProductSpinner from "vue-product-spinner";
+
+
+const images = [...Array(30)].map(
+  (_, i) =>
+    `https://jcastillovnz.github.io/montigny/iframes/roof/renders/${i +
+      1}.jpg`
+);
+
+
+
+export default {
+  name: "App",
+  components: {
+    VueProductSpinner
+  },
+  data() {
+    return {
+      imgs: images
+    };
+  }
+};
+</script>
+
+<style >
+
+@import '/../src/css/estilos.css';
+.vue-product-spinner,
+.vue-product-spinner img {
+  max-width: 100%;
+  margin-left: 5%;
+}
+
+</style>
